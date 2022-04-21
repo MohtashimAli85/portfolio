c646de7fa07650c3b25e06643aeb2a812c1f6e08
MohtashimAli85 <mohtashima85@gmail.com>
2022-04-22 04:46:06 +0500

    created layout, navbar and footer component

---
+++ components/layout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./navbar";
+import Footer from "./footer";
+export default function Layout({ children }) {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+}
