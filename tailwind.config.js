430f8650af06d0ba3a5497dd7eb76d2c2dd073ee
MohtashimAli85 <mohtashima85@gmail.com>
2022-04-22 04:44:41 +0500

    installed tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      aqua: "#25F3C2",
+      "baltic-sea": "#262626",
+      "white-smoke": "#F4F5F7",
+      "rangoon-green": "#1B1B1B",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+  },
+  plugins: [],
+};
