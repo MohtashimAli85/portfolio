79be2d1e2cfc706e82ed594dea844c6251b9f16e
MohtashimAli85 <mohtashima85@gmail.com>
2022-04-27 00:16:02 +0500

    added blob shape

--- components/header.jsx
+++ components/header.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-
+import TopBlob from "./blobs/topblob";
 function Header({ title, role, country }) {
   return (
-    <div className="dark:text-white text-center mt-4 mb-20">
-      <h1 className="text-[40px] leading-[48.13px]">{title}</h1>
-      <h2 className="text-[40px] leading-[48.13px] my-[5px]">{role}</h2>
-      <p className="text-2xl dark:text-white/90 leading-7">
-        Based in {country}
-      </p>
-    </div>
+    <>
+      <div className="dark:text-white text-center mt-4 mb-16">
+        <h1 className="text-[40px] leading-[48.13px]">{title}</h1>
+        <h2 className="text-[40px] leading-[48.13px] my-[5px]">{role}</h2>
+        <p className="text-2xl dark:text-white/90 leading-7">
+          Based in {country}
+        </p>
+      </div>
+      <TopBlob />
+    </>
   );
 }
 
