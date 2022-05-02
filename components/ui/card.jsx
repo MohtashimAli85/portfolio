dc7884d1ce3b92405d79abb6c20466b6a1c54740
MohtashimAli85 <mohtashima85@gmail.com>
2022-05-02 18:27:25 +0500

    made design fixes in card component

--- components/ui/card.jsx
+++ components/ui/card.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-function Card({ img }) {
+function Card({ img, title, href, type, description }) {
   return (
-    <div className="pb-6 border-b border-b-white">
-      <h1 className="text-sm tracking-[2px] leading-[18px]">WEBSITE DESIGN</h1>
-      <Image src={img} width={508} height={401} alt={title} className="my-6" />
-      <div>
-        <h2 className="font-extrabold">
-          <span className="text-primary">{title}</span> <span>{type}</span>
+    <div className="pb-6 border-b border-b-white max-w-[504px]">
+      <h1 className="dark:text-white text-sm tracking-[2px] leading-[18px] font-extrabold mb-6">
+        WEBSITE DESIGN
+      </h1>
+      <Image src={img} width={508} height={401} alt={title} />
+      <div className="flex justify-between items-center my-6">
+        <h2 className="text-2xl leading-10">
+          <span className="text-primary font-extrabold">{title}</span>{" "}
+          <span className="dark:text-white">- {type}</span>
         </h2>
         <Link href={href}>
-          <a>Visit</a>
+          <a className="bg-primary text-sm px-7 py-3 rounded">Visit</a>
         </Link>
       </div>
       <p className="text-white/80 mt-4">{description}</p>
