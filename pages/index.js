795efa99f032f376a7812735df04c882ab791087
MohtashimAli85 <mohtashima85@gmail.com>
2022-05-02 18:28:52 +0500

    added project section

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Layout from "../components/layout";
 import Header from "../components/header";
 import HeroSection from "../components/herosection/herosection";
+import Projects from "../components/projects";
 // import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -20,6 +21,7 @@ export default function Home() {
         country="Pakistan"
       />
       <HeroSection />
+      <Projects />
     </Layout>
   );
 }
