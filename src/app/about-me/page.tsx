17f43e97e6ae7b97fa9c4458ff2e3cc26b7e7e64
Mohtashim Ali <mohtashima85@gmail.com>
2024-02-02 19:43:37 +0500

    created pages

---
+++ src/app/about-me/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutMe() {
+    return (
+      <main className='py-24 max-w-[65%] mx-auto'>
+        about me
+      </main>
+    );
+  }
+  
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,8 @@
+export default function Projects() {
+    return (
+      <div className='py-24 max-w-[65%] mx-auto'>
+        projects
+      </div>
+    );
+  }
+  
