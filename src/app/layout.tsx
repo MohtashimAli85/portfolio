f26617fb3ec4c17e173ef5c6de73f9e3bc4e63e7
Mohtashim Ali <mohtashima85@gmail.com>
2024-02-02 19:43:43 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,25 @@
-import type { Metadata } from "next";
-import { Fira_Code } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Fira_Code } from 'next/font/google';
+import './globals.css';
+import Layout from './components/layout';
 
-const fira_code = Fira_Code({ subsets: ["latin"] });
+const fira_code = Fira_Code({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Mohtashim Ali | Portfolio",
-  description: "Developed by Mohtashim Ali",
+  title: 'Mohtashim Ali | Portfolio',
+  description: 'Developed by Mohtashim Ali'
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${fira_code.className} bg-primary`}>{children}</body>
+    <html lang='en'>
+      <body className={`${fira_code.className} flex flex-col justify-between bg-primary`}>
+        <Layout>{children}</Layout>
+      </body>
     </html>
   );
 }
