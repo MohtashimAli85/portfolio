b73522ff924c8eda538c15df49f5d7ff36753364
Mohtashim Ali <mohtashima85@gmail.com>
2024-02-08 15:30:52 +0500

    page(helllo): integrated bg as shadow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import Shadow from "./components/ui/Shadow";
+
 export default function Home() {
   return (
     <section className='flex items-center h-full px-5 md:px-0 py-20 md:py-24 md:ml-[15%] md:mr-[10%]  md:max-w-[75%] '>
       <div className='flex h-full max-h-[510px] w-full'>
-        <div className='flex flex-col justify-between basis-1/2 md:space-y-20'>
+        <div className='flex flex-col relative -z-0 justify-between md:basis-1/2 md:space-y-20'>
+            <Shadow color="primary" top={0} left={-15} />
+            <Shadow color="secondary" top={150} right={-15} />
           <div className='space-y-3'>
             <p className='text-base md:text-xl'>Hi all, I am</p>
             <h1 className='text-6xl leading-[1.25]'>Mohtashim Ali</h1>
@@ -11,18 +15,18 @@ export default function Home() {
             </h2>
           </div>
           <div className=' text-sm md:text-base'>
-            <p className='text-secondary-dark'>
+            <p className='text-secondary-dark break-words'>
               {'//'} you can also check it on my GitHub page
             </p>
-            <h3 className='font-medium text-salmon max-w-[90vw] truncate'>
+            <h3 className='font-medium text-salmon break-all '>
               <span className='text-secondary'>const </span>
               <span className='text-aquamarine'> githubLink </span>
-              <span className="text-white">=</span>
-              <span className='text-salmon'>
+              <span className="text-white">= </span>
+              <span className='text-salmon '>
                 <a
                   href='https://github.com/MohtashimAli85/portfolio'
                   target='_blank'
-                  className='truncate'
+                  className='break-words'
                 >
                   &quot;https://github.com/MohtashimAli85/portfolio&quot;
                 </a>
