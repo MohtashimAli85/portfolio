97f68f58f8c938c8a9639611bd10c2c95990531a
Mohtashim Ali <mohtashima85@gmail.com>
2024-02-08 15:51:22 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
-import Shadow from "./components/ui/Shadow";
+import Shadow from './components/ui/Shadow';
 
 export default function Home() {
   return (
     <section className='flex items-center h-full px-5 md:px-0 py-20 md:py-24 md:ml-[15%] md:mr-[10%]  md:max-w-[75%] '>
       <div className='flex h-full max-h-[510px] w-full'>
-        <div className='flex flex-col relative -z-0 justify-between md:basis-1/2 md:space-y-20'>
-            <Shadow color="primary" top={0} left={-15} />
-            <Shadow color="secondary" top={150} right={-15} />
+        <div className='flex flex-col relative -z-0 justify-between w-full md:basis-1/2 md:space-y-20'>
+          <Shadow color='primary' top={0} left={-15} className=' md:hidden' />
+          <Shadow
+            color='secondary'
+            top={150}
+            right={-15}
+            className='md:hidden'
+          />
           <div className='space-y-3'>
             <p className='text-base md:text-xl'>Hi all, I am</p>
             <h1 className='text-6xl leading-[1.25]'>Mohtashim Ali</h1>
@@ -21,7 +26,7 @@ export default function Home() {
             <h3 className='font-medium text-salmon break-all '>
               <span className='text-secondary'>const </span>
               <span className='text-aquamarine'> githubLink </span>
-              <span className="text-white">= </span>
+              <span className='text-white'>= </span>
               <span className='text-salmon '>
                 <a
                   href='https://github.com/MohtashimAli85/portfolio'
