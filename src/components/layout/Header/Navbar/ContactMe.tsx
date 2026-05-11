e4d34a75f0ae57e13ae873a8888809421cf6fac8
Mohtashim Ali <mohtashima85@gmail.com>
2026-05-11 22:00:57 +0500

    Refactor ContactMe component to use route object for Item props

--- src/components/layout/Header/Navbar/ContactMe.tsx
+++ src/components/layout/Header/Navbar/ContactMe.tsx
@@ -5,7 +5,12 @@ import Item from "./Item";
 const ContactMe = () => {
   return (
     <div className="hidden lg:block text-secondary-dark shrink-0 [&>li]:w-full [&>li]:block [&>li>a]:block [&>li]:border-r-0!  [&>li>a]:py-[14px]  whitespace-nowrap">
-      <Item href="/contact-me" />
+      <Item
+        route={{
+          href: "/contact-me",
+          name: "Contact Me",
+        }}
+      />
     </div>
   );
 };
