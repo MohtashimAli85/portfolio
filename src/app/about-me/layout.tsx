2ce0e8fc507af3aef3cfe01111cc2f22de4bd5ed
Mohtashim Ali <mohtashima85@gmail.com>
2026-05-12 21:04:09 +0500

    Add @remixicon/react dependency and update AboutMeLayout with icons for sidebar items

--- src/app/about-me/layout.tsx
+++ src/app/about-me/layout.tsx
@@ -1,26 +1,39 @@
+import {
+  RemixiconComponentType,
+  RiTerminalBoxFill,
+  RiUser4Fill,
+} from "@remixicon/react";
+import { Route } from "next";
+import Link from "next/link";
 
-const sidebarItems= [
+const sidebarItems: {
+  name: string;
+  href: Route;
+  icon: RemixiconComponentType;
+}[] = [
+  {
+    name: "Professional Experience",
+    href: "/about-me/professional-info",
+    icon: RiTerminalBoxFill,
+  },
   {
     name: "Personal Info",
-    href: "/about-me/personal-info"
+    href: "/about-me/personal-info",
+    icon: RiUser4Fill,
   },
-  {
-    name: "Education",
-    href: "/about-me/education"
-  },
-  {
-    name: "Experience",
-    href: "/about-me/experience"
-  },
-  {
-    name: "Skills",
-    href: "/about-me/skills"
-  },
-] 
+];
 const AboutMeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex gap-2">
-      sidebar
+    <div className="flex gap-2 h-full">
+      <ul className="py-3 px-6 border-r space-y-4 ">
+        {sidebarItems.map((item) => (
+          <li key={item.href}>
+            <Link key={item.href} href={item.href} title={item.name}>
+              <item.icon />
+            </Link>
+          </li>
+        ))}
+      </ul>
       {children}
     </div>
   );
