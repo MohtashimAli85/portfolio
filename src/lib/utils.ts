0d2c0b25292af70025f95e4b5b429a0a98c0a2f4
Mohtashim Ali <mohtashima85@gmail.com>
2026-07-04 14:07:23 +0500

    Refactor utils: improve code formatting and consistency

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,16 @@
 export function cn(...c: (string | undefined | boolean)[]): string {
-  return c.filter(Boolean).join(" ");
+	return c.filter(Boolean).join(" ");
+}
+
+export const getRouteSegments = (pathname: string) =>
+	pathname.split("/").filter(Boolean);
+
+export function objectToArray<T extends object, K extends string>(
+	obj: Record<string, T>,
+	keyName: K,
+) {
+	return Object.entries(obj).map(([key, value]) => ({
+		[keyName]: key,
+		...value,
+	})) as unknown as (T & Record<K, string>)[];
 }
