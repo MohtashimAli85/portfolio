f0c8db131418d621e3dc0e7006cceb3f2678471b
Mohtashim Ali <mohtashima85@gmail.com>
2026-07-04 14:07:57 +0500

    Refactor Header and Navbar: enhance layout structure and improve code organization

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,11 +1,10 @@
-import React from "react";
 import Navbar from "./navbar";
 import ContactMe from "./navbar/contact-me";
 
 const Header = () => {
 	return (
 		<header className=" h-14   border-b border-theme-stroke flex items-center justify-between lg:justify-normal">
-			<h1 className="text-theme-foreground md:basis-80 pl-5.5 relative z-20">
+			<h1 className="text-theme-foreground md:basis-80 h-full leading-13.75 pl-5.5 relative z-20 border-r border-theme-stroke">
 				mohtashim-ali
 			</h1>
 			<Navbar />
--- src/components/layout/header/navbar/index.tsx
+++ src/components/layout/header/navbar/index.tsx
@@ -1,11 +1,11 @@
 "use client";
-import NavItem from "./nav-item";
 import MobileMenu from "./mobile-menu";
+import NavItem from "./nav-item";
 import navbarRoutes from "./routes";
 
 const Navbar = () => {
 	return (
-		<nav className=" lg:grow h-full lg:border-l lg:border-r border-theme-stroke">
+		<nav className=" lg:grow h-full lg:border- lg:border-r border-theme-stroke">
 			<ul className="hidden lg:flex h-full [&>li+li]:border-l [&>li+li]:border-l-theme-stroke">
 				{navbarRoutes.toSpliced(-1).map((route) => (
 					<li key={route.href}>
