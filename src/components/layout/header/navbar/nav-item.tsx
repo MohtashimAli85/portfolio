68b078689110448676c3ebbbf1ce03bf233f1c9b
Mohtashim Ali <mohtashima85@gmail.com>
2026-07-04 14:07:32 +0500

    Refactor NavItem: improve import statements and enhance code readability

--- src/components/layout/header/navbar/nav-item.tsx
+++ src/components/layout/header/navbar/nav-item.tsx
@@ -1,59 +1,62 @@
+import Link from "next/link";
+import type React from "react";
+import type { FC } from "react";
 import useActivePath from "@/hooks/use-active-path";
 import { cn } from "@/lib/utils";
-import Link from "next/link";
-import React, { FC } from "react";
-import { NavbarRoute } from "./routes";
+import type { NavbarRoute } from "./routes";
 
 interface Props extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
-  route: NavbarRoute;
-  isMobile?: boolean;
-  delay?: string;
-  open?: boolean;
+	route: NavbarRoute;
+	isMobile?: boolean;
+	delay?: string;
+	open?: boolean;
 }
 const NavItem: FC<Props> = ({
-  route,
-  isMobile = false,
-  delay,
-  open,
-  className = "",
-  ...rest
+	route,
+	isMobile = false,
+	delay,
+	open,
+	className = "",
+	onClick,
+	...rest
 }) => {
-  const { matches } = useActivePath();
-  const isActive = matches(route.href.split("/")[1] || "/");
-  return (
-    <Link
-      {...rest}
-      style={{
-        animationDelay: delay || "0",
-        animationFillMode: "forwards",
-      }}
-      href={route.href}
-      className={cn(
-        "relative px-7.5 py-3 h-full",
-        isMobile
-          ? cn(
-              "text-2xl uppercase translate-y-4",
-              open ? "opacity-0 animate-fade-in-up" : "animate-fade-out",
-              isActive && "text-primary-background",
-              "hover:text-primary-hover transition-colors ease-in-cubic",
-            )
-          : cn(
-              "grid md:place-content-center last:border-r last:border-r-theme-stroke",
-              !isActive && "text-theme-foreground hover:bg-theme-backdrop/50",
-              "transition-all",
-            ),
-        className,
-      )}
-    >
-      {route.name}
-      {!isMobile && (
-        <hr
-          data-show={isActive}
-          className="hidden data-[show='true']:block w-full bottom-0 absolute text-primary-background bg-primary-background h-1"
-        />
-      )}
-    </Link>
-  );
+	const { matches } = useActivePath();
+	const isActive = matches(route.href.split("/")[1] || "/");
+	return (
+		<Link
+			onClick={onClick}
+			{...rest}
+			style={{
+				animationDelay: delay || "0",
+				animationFillMode: "forwards",
+			}}
+			href={route.href}
+			className={cn(
+				"relative px-7.5 py-3 h-full",
+				isMobile
+					? cn(
+							"text-2xl uppercase translate-y-4",
+							open ? "opacity-0 animate-fade-in-up" : "animate-fade-out",
+							isActive && "text-primary-background",
+							"hover:text-primary-hover transition-colors ease-in-cubic",
+						)
+					: cn(
+							"grid md:place-content-center last:border-r last:border-r-theme-stroke",
+							!isActive && "text-theme-foreground hover:bg-theme-backdrop/50",
+							"transition-all",
+						),
+				className,
+			)}
+		>
+			{route.name}
+			{!isMobile && (
+				<hr
+					data-show={isActive}
+					className="hidden data-[show='true']:block w-full bottom-0 absolute text-primary-background bg-primary-background h-1"
+				/>
+			)}
+		</Link>
+	);
 };
 
 export default NavItem;
