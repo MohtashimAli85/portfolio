8b358bd8ba8c8d2b9e83cea4662725184520ae03
Mohtashim Ali <mohtashima85@gmail.com>
2026-07-04 12:12:46 +0500

    Refactor NavbarRoute: improve code formatting and consistency

--- src/components/layout/header/navbar/routes.ts
+++ src/components/layout/header/navbar/routes.ts
@@ -1,24 +1,24 @@
-import { Route } from "next";
+import type { Route } from "next";
 export interface NavbarRoute {
-  name: string;
-  href: Route;
+	name: string;
+	href: Route;
 }
 const navbarRoutes: NavbarRoute[] = [
-  {
-    name: "_hello",
-    href: "/",
-  },
-  {
-    name: "_about-me",
-    href: "/profile/personal" as Route,
-  },
-  {
-    name: "_projects",
-    href: "/projects",
-  },
-  {
-    name: "_contact-me",
-    href: "/contact-me",
-  },
+	{
+		name: "_hello",
+		href: "/",
+	},
+	{
+		name: "_about-me",
+		href: "/profile/personal/overview" as Route,
+	},
+	{
+		name: "_projects",
+		href: "/projects",
+	},
+	{
+		name: "_contact-me",
+		href: "/contact-me",
+	},
 ];
 export default navbarRoutes;
